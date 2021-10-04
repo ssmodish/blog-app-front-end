ce4b602068c728e6a48b41a8e65ac4b2f407ea5d
Steve Smodish <smodish@hotmail.com>
2021-10-04 18:12:40 -0400

    Stripped create react app and add GH logos

---
+++ .history/src/App_20211004180109.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Welcome to Grammerhub</div>
+}
+
+export default App
---
+++ .history/src/App_20211004180211.js
@@ -0,0 +1,11 @@
+function App() {
+  return {
+  < div >
+    <h1>
+      Welcome to Grammerhub
+    </h1>
+  </div >
+  }
+}
+
+export default App
