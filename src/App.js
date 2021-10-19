882fe88a9e1d82b5c892f2285f7ddb2ab400ada0
laddie <ladinakasamuels@gmail.com>
2021-10-19 14:23:02 -0400

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
+import Navbar from "./components/navbar";
+import PostForm from "./components/post-form";
+import PostList from "./components/post-list";
 function App() {
   return (
     <div>
-      <div id="navbar">
-        <h1>Welcome to Grammerhub</h1>
-        <div className="authorization">
-          <button>Login</button>
-          <button>Register</button>
-          <button>Logout</button>
-        </div>
+      <Navbar />
+      <PostList />
+      <PostForm />
+      <div className="greeting">
+        <h2>Hello User!</h2>
       </div>
-      <div className="postList">
-        <ul>
-          <div className="post">
-            <li>
-              Post 1
-              <button>Edit</button>
-              <button>Delete</button>
-            </li>
-          </div>
-          <li>Post 2</li>
-          <li>Post 3</li>
-        </ul>
-      </div>
-      <div className="form">
-        <form>
-          <label for="name">Name:</label>
-          <input type="text" id="name" name="name" /><br />
-          <label for="body">Body:</label>
-          <input type="text" id="body" name="body" /><br />
-          <button>Submit</button>
-        </form>
-      </div>
-        <div className="greeting">
-          <h2>Hello User!</h2>
-        </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
