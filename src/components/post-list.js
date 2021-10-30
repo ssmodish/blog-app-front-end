360ca389615dd70fa2f36850032a6907b9fb8916
Steve Smodish <smodish@hotmail.com>
2021-10-30 13:27:53 -0400

    Fetching data from datbase

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -1,14 +1,32 @@
-import Post from "./post";
+import { useState, useEffect } from 'react'
+
 function PostList() {
+  const [postListData, setPostListData] = useState([])
+  const url = 'http://localhost:8080/api/posts'
+  const fetchPostListData = async () => {
+    try {
+      const response = await fetch(url, { mode: 'cors' })
+      const data = await response.json()
+      console.log({ data })
+      setPostListData(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchPostListData()
+  }, [])
+
   return (
-    <div className="postList">
+    <div className='postList'>
       <ul>
-        <Post />
-        <Post />
-        <Post />
+        {postListData.map((post) => (
+          <li>Post: {post.post_title}</li>
+        ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default PostList;
+export default PostList
